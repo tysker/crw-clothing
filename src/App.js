798864a23913a54d03f0dc85eef745180b8b23be
tysker <tyskerjoerg@gmail.com>
2021-10-10 20:35:25 +0200

    course-content number 78 Router Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import './App.css';
-import Homepage from './component/homepage';
+import React from "react";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.jsx";
+import { Switch, Route } from "react-router-dom";
+
+const HatPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+const HatPage2 = () => {
+  return (
+    <div>
+      <p>Hallo from hats</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hat" component={HatPage} />
+        <Route path="/hats" component={HatPage2} />
+      </Switch>
+      {/* <Homepage /> */}
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import Directory from '../../component/directory/directory';
+
+import './hompage.style.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+    
+);
+
+export default Homepage;
